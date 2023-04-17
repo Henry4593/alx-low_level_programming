f37bdab1fdac1f879bebb81507561dc2270067bd
Henry4593 <jonyango4@gmail.com>
2023-04-18 00:51:28 +0300

    solved tasks  1-init_dog.c through 5-free_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,35 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - its members are attributes of
  * a dog.
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ *
+ * Description - everything description about dog
+ * goes here
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+/**
+ * dog_t - alias of struct dog(typedef)
+ *
+ */
+
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
 
 #endif
